21c78c3ab5ed37b82f6af49dbea35eaeb50f8ae6
VinhVoDuy <vinh.vo@2359media.com>
2017-04-02 18:38:31 +0700

    landing image
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- app/models/landing_image.rb
+++ app/models/landing_image.rb
@@ -1,4 +1,4 @@
 class LandingImage < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "1700x449>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
