d47e656f31074138c9acfacfb0f9aa96c905154a
VinhVoDuy <vinh.vo@2359media.com>
2017-05-03 02:07:33 +0700

    change asset-host
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,13 +28,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = Proc.new { |source|
-    if source.match(/.*assets.*/)
-      "/"
-    else
-      "https://d2lhmq651ffnjt.cloudfront.net"
-    end
-  }
+  # config.action_controller.asset_host =
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
