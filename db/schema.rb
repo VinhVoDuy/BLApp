d84f74eb60aa9f695d4e034a99ea74d30db2595e
VinhVoDuy <vinh.vo@2359media.com>
2017-06-13 16:51:00 +0700

    tours
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608103542) do
+ActiveRecord::Schema.define(version: 20170613094613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -186,6 +186,7 @@ ActiveRecord::Schema.define(version: 20170608103542) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "short_description"
+    t.string   "slug"
   end
 
   create_table "users", force: :cascade do |t|
