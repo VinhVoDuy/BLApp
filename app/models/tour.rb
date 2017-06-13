e76a24446af33011c289d3b505581115de9dd371
VinhVoDuy <vinh.vo@2359media.com>
2017-06-13 16:43:02 +0700

    tours added
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,11 @@
 class Tour < ApplicationRecord
   has_many :tour_images
+  friendly_id :name, use: :slugged
   accepts_nested_attributes_for :tour_images, allow_destroy: true, reject_if: :all_blank
   PRIVATE_GROUP = ['2', '3 - 4', '5 - 7', '8 - 9', '10 +']
+
+  private
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
 end
