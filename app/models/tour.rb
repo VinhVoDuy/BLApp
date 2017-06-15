1654a6ae7bbd693044a65313e0114402faa432e1
VinhVoDuy <vinh.vo@2359media.com>
2017-06-16 04:13:21 +0700

    update tours
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -6,8 +6,12 @@ class Tour < ApplicationRecord
   accepts_nested_attributes_for :tour_images, allow_destroy: true, reject_if: :all_blank
   PRIVATE_GROUP = ['2', '3 - 4', '5 - 7', '8 - 9', '10 +']
 
+  def class_tag_list
+    tag_list.map(&:downcase).map{|e| e.gsub(' ', '_')}.join(' ')
+  end
   private
   def should_generate_new_friendly_id?
     slug.blank? || name_changed?
   end
+
 end
