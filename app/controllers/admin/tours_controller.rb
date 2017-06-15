3dcbf7f3f3de9dced6f3dadb83274106af5222c0
VinhVoDuy <vinh.vo@2359media.com>
2017-06-16 03:59:15 +0700

    remove tour
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- app/controllers/admin/tours_controller.rb
+++ app/controllers/admin/tours_controller.rb
@@ -30,6 +30,12 @@ class Admin::ToursController < AdminController
     end
   end
 
+  def destroy
+    @tour = Tour.friendly.find params[:id]
+    @tour.destroy
+    redirect_to action: :index
+  end
+
   private
   def tour_params
     params.require(:tour).permit(:group, :tag_list, :name, :start_time, :end_time, :price, :included, :short_description, private_tour_pricing: [], tour_images_attributes: [:id, :image, :description, :primary, :_destroy])
