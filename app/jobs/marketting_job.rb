79cf7334e0619fc89be66506a68bcdfabf7995f2
VinhVoDuy <vinh.vo@2359media.com>
2017-06-22 22:29:33 +0700

    add google analytic
    Signed-off-by: VinhVoDuy <vinh.vo@2359media.com>

--- app/jobs/marketting_job.rb
+++ app/jobs/marketting_job.rb
@@ -2,7 +2,7 @@ class MarkettingJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Contact.find_each do |contact|
+    Contact.where('id > 25').find_each do |contact|
       MarkettingMailer.launch_web(contact).deliver_later(wait: (5.minutes * contact.id))
     end
   end
